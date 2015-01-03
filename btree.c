46b49fc2904cc92e93693da2c076d020e6b2518a
andreydanil <adanilko@hawk.iit.edu>
2015-01-02 21:21:37 -0600

    interview study
    initial commit

---
+++ btree.c
@@ -0,0 +1,31 @@
+// Binary tree
+// hierarchal data: used for file system
+// organize data: search, insertion, deletion
+// trie - dictionary
+// network routing algorithm
+
+// Array
+// search(x) - O(n)
+// insert(x) - O(1)
+// remove(x) - O(n)
+
+// Linked list
+// search(x) - O(n)
+// insert(x) - O(1)
+// remove(x) - O(n)
+
+// BST
+// search(x) - O(log n)
+// insert(x) - O(log n)
+// remove(x) - O(log n)
+
+// Binary search
+// sorted array search- O(logn)
+
+
+struct Node {
+	int data;
+	Node * left;
+	Node * right;
+};
+
